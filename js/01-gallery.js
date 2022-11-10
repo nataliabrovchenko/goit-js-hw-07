995f53ef416b4a30c9c8a016fe058749655119eb
nataliabrovchenko <nats.brovchenko@gmail.com>
2022-11-10 17:16:08 +0200

    add makeGalleryItems

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(`.gallery`);
+
+galleryContainer.insertAdjacentHTML(`afterbegin`, createGalleryItems(galleryItems));
+
+// galleryContainer.addEventListener(`click`, onGalleryContainerClick);
+
+console.log(createGalleryItems(galleryItems));
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `    
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+                </div>
+            `;
+        })
+        .join(``);
+}
+
+function createModalWindow(src) {
+    const instance = basicLightbox.create(
+    `
+    <img src="${src}>
+    `);
+    instance.show()
+}
